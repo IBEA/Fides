246b2c56d4faf7cdc74f96e63713a71b5e7f5f96
Alaina Traxler <eloavox@gmail.com>
2017-01-25 13:17:37 -0800

    commit basic setup for HomeActivity and put a temp button in for redirects.

--- app/src/main/java/com/ibea/fides/ui/HomeActivity.java
+++ app/src/main/java/com/ibea/fides/ui/HomeActivity.java
@@ -1,15 +1,34 @@
 package com.ibea.fides.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.ibea.fides.BaseActivity;
 import com.ibea.fides.R;
 
-public class HomeActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class HomeActivity extends BaseActivity implements View.OnClickListener {
+    @Bind(R.id.button_Dirty) Button mButton_Dirty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+
+        mButton_Dirty.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == mButton_Dirty){
+            Intent intent = new Intent(mContext, ShiftsCreateActivity.class);
+            startActivity(intent);
+        }
     }
 }
