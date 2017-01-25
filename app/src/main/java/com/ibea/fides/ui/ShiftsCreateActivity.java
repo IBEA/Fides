4a21659d1c4060e2e7a13d93d34e4f7539459d00
Alaina Traxler <eloavox@gmail.com>
2017-01-25 10:52:35 -0800

    add ShiftsCreateActivity

---
+++ app/src/main/java/com/ibea/fides/ui/ShiftsCreateActivity.java
@@ -0,0 +1,13 @@
+package com.ibea.fides.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShiftsCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shifts_create);
+    }
+}
