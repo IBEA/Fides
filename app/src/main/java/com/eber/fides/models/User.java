2d6e92a888119168649a09b77ba45ef9fd2acfac
Justin <justin.m.kincaid@gmail.com>
2017-01-24 19:14:37 -0800

    create basic User model for Account Registration

--- app/src/main/java/com/eber/fides/models/User.java
+++ app/src/main/java/com/eber/fides/models/User.java
@@ -5,4 +5,30 @@ package com.eber.fides.models;
  */
 
 public class User {
+    String pushId;
+    String name;
+
+    // Empty Constructor for Parceler
+    public User() {}
+
+    // Basic Constructor
+    public User(String pushId, String name) {
+        this.pushId = pushId;
+        this.name = name;
+    }
+
+    // Getters and Setters
+    public String getPushId() {
+        return pushId;
+    }
+    public void setPushId(String pushId) {
+        this.pushId = pushId;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
