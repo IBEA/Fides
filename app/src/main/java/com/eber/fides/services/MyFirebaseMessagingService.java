6a849918e6b4b008597840bdc4fb823bdf871af9
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2017-01-25 11:43:07 -0800

    add code for notifcations

--- app/src/main/java/com/eber/fides/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/eber/fides/services/MyFirebaseMessagingService.java
@@ -1,9 +1,16 @@
 package com.eber.fides.services;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.ibea.fides.BaseActivity;
+import com.ibea.fides.R;
 
 /**
  * Created by Garrett on 1/25/2017.
@@ -13,10 +20,18 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FCM Service";
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO: Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated.
+        Intent intent = new Intent(this,BaseActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder notifactionBuilder = new NotificationCompat.Builder(this);
+        notifactionBuilder.setContentTitle("FCM Notification");
+        notifactionBuilder.setContentText(remoteMessage.getNotification().getBody());
+        notifactionBuilder.setAutoCancel(true);
+        notifactionBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        notifactionBuilder.setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notifactionBuilder.build());
+
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
     }
