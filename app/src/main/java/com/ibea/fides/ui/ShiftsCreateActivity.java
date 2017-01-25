438ac5e2e8f8700f4743c679614be5134de37f58
Alaina Traxler <eloavox@gmail.com>
2017-01-25 12:33:06 -0800

    implement OnClickListener on ShiftsCreate and attach to button

--- app/src/main/java/com/ibea/fides/ui/ShiftsCreateActivity.java
+++ app/src/main/java/com/ibea/fides/ui/ShiftsCreateActivity.java
@@ -2,19 +2,24 @@ package com.ibea.fides.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ibea.fides.BaseActivity;
 import com.ibea.fides.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class ShiftsCreateActivity extends BaseActivity {
+public class ShiftsCreateActivity extends BaseActivity implements View.OnClickListener{
     @Bind(R.id.editText_From) EditText mEditText_From;
     @Bind(R.id.editText_Until) EditText mEditText_Until;
     @Bind(R.id.editText_Date) EditText mEditText_Date;
     @Bind(R.id.editText_MaxVolunteers) EditText mEditText_MaxVolunteers;
+    @Bind(R.id.button_LetsGo) Button mButton_LetsGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +27,13 @@ public class ShiftsCreateActivity extends BaseActivity {
         setContentView(R.layout.activity_shifts_create);
         ButterKnife.bind(this);
 
+        mButton_LetsGo.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v == mButton_LetsGo){
+            Toast.makeText(mContext, "Pop", Toast.LENGTH_SHORT).show();
+        }
     }
 }
