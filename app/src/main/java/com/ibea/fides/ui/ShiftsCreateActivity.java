63e3e13d8727a47da548f0410e69c3ffa98c7137
Alaina Traxler <eloavox@gmail.com>
2017-01-25 12:29:28 -0800

    add and bind basic layout for ShiftsCreate.

--- app/src/main/java/com/ibea/fides/ui/ShiftsCreateActivity.java
+++ app/src/main/java/com/ibea/fides/ui/ShiftsCreateActivity.java
@@ -2,14 +2,25 @@ package com.ibea.fides.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.ibea.fides.BaseActivity;
 import com.ibea.fides.R;
 
-public class ShiftsCreateActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class ShiftsCreateActivity extends BaseActivity {
+    @Bind(R.id.editText_From) EditText mEditText_From;
+    @Bind(R.id.editText_Until) EditText mEditText_Until;
+    @Bind(R.id.editText_Date) EditText mEditText_Date;
+    @Bind(R.id.editText_MaxVolunteers) EditText mEditText_MaxVolunteers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shifts_create);
+        ButterKnife.bind(this);
+
     }
 }
