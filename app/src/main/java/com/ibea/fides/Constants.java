9eada826301e580f8f80518e977873fe41139a10
Alaina Traxler <eloavox@gmail.com>
2017-01-26 11:07:15 -0800

    add Constant and BaseAcivity variable for shiftsAvailable node.

--- app/src/main/java/com/ibea/fides/Constants.java
+++ app/src/main/java/com/ibea/fides/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
     public static final String DB_NODE_SHIFTS = "shifts";
     public static final String DB_NODE_ORGANIZATIONS = "organizations";
     public static final String DB_NODE_TAGS = "tags";
+    public static final String DB_NODE_SHIFTSAVAILABLE = "shiftsAvailable";
 
     // Constants for fields. Reusable if fields are named same.
     //OID == Organization ID, as provided by the API
