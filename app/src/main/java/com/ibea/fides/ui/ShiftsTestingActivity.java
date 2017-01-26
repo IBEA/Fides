14862439f385dc845f92af2636717b558bde2653
Alaina Traxler <eloavox@gmail.com>
2017-01-25 17:30:43 -0800

    add RecyclerView dependency and add to ShiftsTesting.

--- app/src/main/java/com/ibea/fides/ui/ShiftsTestingActivity.java
+++ app/src/main/java/com/ibea/fides/ui/ShiftsTestingActivity.java
@@ -2,6 +2,7 @@ package com.ibea.fides.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
