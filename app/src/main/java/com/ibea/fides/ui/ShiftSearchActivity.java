764b65ec443f0b659199f8f2b41d4d34059a9739
Justin <KincaidJ@Justins-Air.T-mobile.com>
2017-01-26 16:41:32 -0800

    display requested shifts by zipcode

--- app/src/main/java/com/ibea/fides/ui/ShiftSearchActivity.java
+++ app/src/main/java/com/ibea/fides/ui/ShiftSearchActivity.java
@@ -2,28 +2,40 @@ package com.ibea.fides.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.ibea.fides.BaseActivity;
+import com.ibea.fides.Constants;
 import com.ibea.fides.R;
+import com.ibea.fides.adapters.DirtyFirebaseShiftViewHolder;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class ShiftSearchActivity extends BaseActivity implements View.OnClickListener{
-    @Bind(R.id.zipcodeRadio) RadioButton mZipRadio;
-    @Bind(R.id.organizationRadio) RadioButton mOrgRadio;
-    @Bind(R.id.searchButton) Button mSearchButton;
-    @Bind(R.id.searchTextInput) EditText mSearchInput;
-    @Bind(R.id.shiftSearchResultsRecyclerView) RecyclerView mRecyclerView;
+public class ShiftSearchActivity extends BaseActivity implements View.OnClickListener {
+    @Bind(R.id.zipcodeRadio)
+    RadioButton mZipRadio;
+    @Bind(R.id.organizationRadio)
+    RadioButton mOrgRadio;
+    @Bind(R.id.searchButton)
+    Button mSearchButton;
+    @Bind(R.id.searchTextInput)
+    EditText mSearchInput;
+    @Bind(R.id.shiftSearchResultsRecyclerView)
+    RecyclerView mRecyclerView;
 
     // Tracks which radio button is checked
     private String mSearchParameter;
     private String zipcode;
+
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +51,7 @@ public class ShiftSearchActivity extends BaseActivity implements View.OnClickLis
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.zipcodeRadio:
                 if (checked)
                     mSearchParameter = "zip";
@@ -53,20 +65,20 @@ public class ShiftSearchActivity extends BaseActivity implements View.OnClickLis
 
     @Override
     public void onClick(View view) {
-        if(view == mSearchButton) {
+        if (view == mSearchButton) {
             zipcode = mSearchInput.getText().toString();
-
-            dbShiftsZip = dbShiftsAvailable.child("zipcode").child(zipcode);
 
             setUpFirebaseAdapter();
         }
     }
 
     private void setUpFirebaseAdapter() {
-        mFirebaseAdapter = new FirebaseShiftSearchAdapter(Shift.class, R.layout.dirty_shift_list_item, DirtyFirebaseShiftViewHolder.class, dbShiftsZip) {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<String, DirtyFirebaseShiftViewHolder>
+                (String.class, R.layout.dirty_shift_list_item, DirtyFirebaseShiftViewHolder.class, dbShiftsAvailable.child(Constants.DB_SUBNODE_ZIPCODE).child(zipcode)) {
+
             @Override
-            protected void populateViewHolder(DirtyFirebaseShiftViewHolder viewHolder, Shift model, int position) {
-                viewHolder.bindShift(model);
+            protected void populateViewHolder(DirtyFirebaseShiftViewHolder viewHolder, String shiftID, int position) {
+                viewHolder.bindShift(shiftID);
             }
         };
         mRecyclerView.setHasFixedSize(true);
@@ -78,3 +90,4 @@ public class ShiftSearchActivity extends BaseActivity implements View.OnClickLis
     protected void onDestroy() {
         super.onDestroy();
     }
+}
