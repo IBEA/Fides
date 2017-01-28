3418bb5480933e09e38469d6c3ab990554fc20e0
Alaina Traxler <eloavox@gmail.com>
2017-01-28 15:31:23 -0800

    add checkpoint for dynamic search.

---
+++ app/src/main/java/com/ibea/fides/adapters/OrganizationListAdapter.java
@@ -0,0 +1,55 @@
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ibea.fides.R;
+import com.ibea.fides.models.Organization;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+
+public class OrganizationListAdapter extends RecyclerView.Adapter<OrganizationListAdapter.OrganizationViewHolder> {
+    private ArrayList<Organization> mOrganizations = new ArrayList<>();
+    private Context mContext;
+
+    public OrganizationListAdapter(Context context, ArrayList<Organization> organizations) {
+        mContext = context;
+        mOrganizations = organizations;
+    }
+
+    @Override
+    public OrganizationListAdapter.OrganizationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.organization_list_item, parent, false);
+        OrganizationViewHolder viewHolder = new OrganizationViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(OrganizationListAdapter.OrganizationViewHolder holder, int position) {
+        holder.bindOrganization(mOrganizations.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mOrganizations.size();
+    }
+
+    public class OrganizationViewHolder extends RecyclerView.ViewHolder {
+        private Context mContext;
+
+        public OrganizationViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindOrganization(Organization organization) {
+            mNameTextView.setText(organization.getName());
+            mCategoryTextView.setText(organization.getCategories().get(0));
+            mRatingTextView.setText("Rating: " + organization.getRating() + "/5");
+        }
+    }
+}
