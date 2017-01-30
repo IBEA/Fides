ba76a9c6600b2c679e19dd4596030b5a963e3162
Justin <justin.m.kincaid@gmail.com>
2017-01-30 14:19:13 -0800

    cleanup firebase naming conventions

---
+++ app/src/main/java/com/ibea/fides/utils/OnItemClickListener.java
@@ -0,0 +1,11 @@
+package com.ibea.fides.utils;
+
+import android.view.View;
+
+/**
+ * Created by KincaidJ on 1/30/17.
+ */
+
+public interface OnItemClickListener {
+    public void onClick(View view, int position);
+}
