68bb92f1c9e78a830aea065d8bf6740a6da72235
Alaina Traxler <eloavox@gmail.com>
2017-01-30 13:30:00 -0800

    add fragment for ShiftsPending when user is a volunteer.

--- app/src/main/java/com/ibea/fides/ui/ShiftsPendingForVolunteerFragment.java
+++ app/src/main/java/com/ibea/fides/ui/ShiftsPendingForVolunteerFragment.java
@@ -2,18 +2,41 @@ package com.ibea.fides.ui;
 
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.ibea.fides.Constants;
 import com.ibea.fides.R;
+import com.ibea.fides.adapters.DirtyFirebaseShiftViewHolder;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ShiftsPendingForVolunteerFragment extends Fragment {
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    private RecyclerView.Adapter mRecyclerAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private FirebaseUser mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+    private DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+    private  DatabaseReference dbShiftsPendingForUser = dbRef.child(Constants.DB_NODE_SHIFTSPENDING).child(Constants.DB_SUBNODE_VOLUNTEERS).child(mCurrentUser.getUid());
 
     public ShiftsPendingForVolunteerFragment() {
         // Required empty public constructor
@@ -24,7 +47,45 @@ public class ShiftsPendingForVolunteerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shifts_pending_for_volunteer, container, false);
+        View view = inflater.inflate(R.layout.fragment_shifts_pending_for_volunteer, container, false);
+        ButterKnife.bind(this, view);
+
+        Log.v(">>>>>", "ShiftsPending current user = " + mCurrentUser.getUid());
+        Log.v(">>>>>", "In onCreateView for ShiftsPending");
+
+        setUpFirebaseAdapter();
+        return view;
+    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    // newInstance constructor for creating fragment with arguments
+    public static ShiftsPendingForVolunteerFragment newInstance(int page, String title) {
+        ShiftsPendingForVolunteerFragment fragmentFirst = new ShiftsPendingForVolunteerFragment();
+        return fragmentFirst;
+    }
+
+    private void setUpFirebaseAdapter() {
+
+        Log.v(">>>>>", "In setupFirebaseAdapter for ShiftsPendingUsers");
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<String, DirtyFirebaseShiftViewHolder>
+                (String.class, R.layout.dirty_shift_list_item, DirtyFirebaseShiftViewHolder.class, dbShiftsPendingForUser) {
+
+            @Override
+            protected void populateViewHolder(DirtyFirebaseShiftViewHolder viewHolder, String shiftID, int position) {
+                Log.v(">>>>>", shiftID);
+                viewHolder.bindShift(shiftID);
+            }
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
     }
 
 }
