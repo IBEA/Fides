964e6b7db846a50ceeb2f915ac03d8b56854180b
Alaina Traxler <eloavox@gmail.com>
2017-01-31 10:16:16 -0800

    add ButterKnife to ShiftsCompletedForVolunteers

--- app/src/main/java/com/ibea/fides/ui/ShiftsCompletedForVolunteersFragment.java
+++ app/src/main/java/com/ibea/fides/ui/ShiftsCompletedForVolunteersFragment.java
@@ -3,16 +3,23 @@ package com.ibea.fides.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.ibea.fides.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ShiftsCompletedForVolunteersFragment extends Fragment {
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+    FirebaseRecyclerAdapter mFirebaseAdapter;
 
 
     public ShiftsCompletedForVolunteersFragment() {
@@ -30,7 +37,9 @@ public class ShiftsCompletedForVolunteersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shifts_completed_for_volunteers, container, false);
+        View view = inflater.inflate(R.layout.fragment_shifts_completed_for_volunteers, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
 }
