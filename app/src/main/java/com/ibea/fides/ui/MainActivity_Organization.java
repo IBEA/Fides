0822d1beba1ef4df28a22b1cb4afab09e09f79e0
Alaina Traxler <eloavox@gmail.com>
2017-02-02 13:26:50 -0800

    add behavior for various tab population scenarios to MainActivityOrganization

--- app/src/main/java/com/ibea/fides/ui/MainActivity_Organization.java
+++ app/src/main/java/com/ibea/fides/ui/MainActivity_Organization.java
@@ -8,14 +8,25 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 
 import com.astuetz.PagerSlidingTabStrip;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ibea.fides.BaseActivity;
 import com.ibea.fides.Constants;
 import com.ibea.fides.R;
 import com.ibea.fides.adapters.UniversalPagerAdapter;
+import com.ibea.fides.models.Organization;
+
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
 public class MainActivity_Organization extends BaseActivity {
+    Organization mOrganization;
+    Boolean isOrganization;
+    String currentUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +35,29 @@ public class MainActivity_Organization extends BaseActivity {
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("username");
-        Boolean isOrganization = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(Constants.KEY_ISORGANIZATION, false);
+        isOrganization = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(Constants.KEY_ISORGANIZATION, false);
 
+        currentUserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        if(intent.getExtras() != null){
+            mOrganization = Parcels.unwrap(intent.getExtras().getParcelable("organization"));
+            populateTabs();
+        }else{
+            FirebaseDatabase.getInstance().getReference().child(Constants.DB_NODE_USERS).child(currentUserId).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    mOrganization = dataSnapshot.getValue(Organization.class);
+                    populateTabs();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    public void populateTabs(){
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
 
         //TODO: This is going to need much more granular parsing
@@ -35,22 +67,28 @@ public class MainActivity_Organization extends BaseActivity {
 
         if(!isOrganization){
             //User is not an organization
+            //TODO: Add aailable shifts fragment
             Log.v(TAG, "User is not an organization");
 
             tabTitles.add("Profile");
             fragmentList.add(new ProfileForOrganizationFragment());
             viewPager.setAdapter(new UniversalPagerAdapter(getSupportFragmentManager(), 1, tabTitles, fragmentList));
         }else {
-            //User is organization, and this is their page
-            Log.v(TAG, "User is an organization");
+            if(mOrganization.getPushId().equals(currentUserId)) {
+                //User is organization, and this is their page
+                Log.v(TAG, "User is an organization");
 
-            tabTitles.add("Profile");
-            tabTitles.add("Upcoming");
-            tabTitles.add("History");
-            fragmentList.add(new ProfileForOrganizationFragment());
-            fragmentList.add(new ShiftsPendingForOrganizationFragment());
-            fragmentList.add(new ShiftsCompletedForOrganizationFragment());
-            viewPager.setAdapter(new UniversalPagerAdapter(getSupportFragmentManager(), 3, tabTitles, fragmentList));
+                tabTitles.add("Profile");
+                tabTitles.add("Upcoming");
+                tabTitles.add("History");
+                fragmentList.add(new ProfileForOrganizationFragment());
+                fragmentList.add(new ShiftsPendingForOrganizationFragment());
+                fragmentList.add(new ShiftsCompletedForOrganizationFragment());
+                viewPager.setAdapter(new UniversalPagerAdapter(getSupportFragmentManager(), 3, tabTitles, fragmentList));
+            }else{
+
+            }
+
         }
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
