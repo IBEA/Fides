6a592b86ec25e4d18c5a6f8649b07811be9dd9e9
Alaina Traxler <eloavox@gmail.com>
2017-02-06 16:53:13 -0800

    add OnQueryTextListener

--- app/src/main/java/com/ibea/fides/ui/NewShiftSearchFragment.java
+++ app/src/main/java/com/ibea/fides/ui/NewShiftSearchFragment.java
@@ -3,17 +3,28 @@ package com.ibea.fides.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.ibea.fides.R;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewShiftSearchFragment extends Fragment {
+    @Bind(R.id.searchView_City) SearchView mSearchView_City;
+    @Bind(R.id.searchView_State) SearchView mSearchView_State;
 
+    DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
 
     public NewShiftSearchFragment() {
         // Required empty public constructor
@@ -23,8 +34,28 @@ public class NewShiftSearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        mSearchView_City.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                mSearchView_City.clearFocus();
+                ArrayList<String> shiftIds = fetchShifts();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_shift_search, container, false);
+    }
+
+    //Retrieves full list of shiftIDs
+    public ArrayList<String> fetchShifts(){
+
     }
 
 }
