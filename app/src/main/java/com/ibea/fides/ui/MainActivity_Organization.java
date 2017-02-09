62925be5c1d9571c05f354544257c18e2edd54df
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2017-02-08 16:50:52 -0800

    add intro for volunteer and organization

--- app/src/main/java/com/ibea/fides/ui/MainActivity_Organization.java
+++ app/src/main/java/com/ibea/fides/ui/MainActivity_Organization.java
@@ -1,6 +1,7 @@
 package com.ibea.fides.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -59,7 +60,41 @@ public class MainActivity_Organization extends BaseActivity {
                 }
             });
         }
+
+        //  Declare a new thread to do a preference check
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //  Initialize SharedPreferences
+                SharedPreferences getPrefs = PreferenceManager
+                        .getDefaultSharedPreferences(getBaseContext());
+
+                //  Create a new boolean and preference and set it to true
+                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+
+                //  If the activity has never started before...
+                if (isFirstStart) {
+
+                    //  Launch app intro
+                    Intent i = new Intent(MainActivity_Organization.this, IntroOrganizationActivity.class);
+                    startActivity(i);
+
+                    //  Make a new preferences editor
+                    SharedPreferences.Editor e = getPrefs.edit();
+
+                    //  Edit preference to make it false because we don't want this to run again
+                    e.putBoolean("firstStart", false);
+
+                    //  Apply changes
+                    e.apply();
+                }
+            }
+        });
+
+        // Start the thread
+        t.start();
     }
+
 
     public void populateTabs(){
         SwipelessViewPager viewPager = (SwipelessViewPager) findViewById(R.id.viewpager);
@@ -102,5 +137,8 @@ public class MainActivity_Organization extends BaseActivity {
         PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         // Attach the view pager to the tab strip
         tabsStrip.setViewPager(viewPager);
+
     }
+
+
 }
