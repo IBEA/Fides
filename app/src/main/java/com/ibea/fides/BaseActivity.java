85f58cafcba1d2b0acad9484136b89a70a63fffa
Alaina Traxler <eloavox@gmail.com>
2017-02-14 09:50:56 -0800

    hide admin option for non-admins

--- app/src/main/java/com/ibea/fides/BaseActivity.java
+++ app/src/main/java/com/ibea/fides/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.ibea.fides;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -14,8 +15,11 @@ import android.widget.AdapterView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.ibea.fides.ui.AdminActivity;
 import com.ibea.fides.ui.LogInActivity;
 import com.ibea.fides.ui.MainActivity_Organization;
@@ -123,6 +127,23 @@ public class BaseActivity extends AppCompatActivity {
         } else{
             inflater.inflate(R.menu.menu_volunteer, menu);
         }
+
+        final MenuItem admin = (MenuItem) menu.findItem(R.id.action_admin);
+
+        dbCurrentUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("isAdmin").getValue(Boolean.class) == false){
+                    admin.setVisible(false);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         return super.onCreateOptionsMenu(menu);
 
     }
