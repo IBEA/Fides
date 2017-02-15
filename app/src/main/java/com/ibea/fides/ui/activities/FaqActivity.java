417c0d1a8742b0dba3f95a467b5601e03ec9c431
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2017-02-15 14:06:02 -0800

    add implicit intent to faq page

--- app/src/main/java/com/ibea/fides/ui/activities/FaqActivity.java
+++ app/src/main/java/com/ibea/fides/ui/activities/FaqActivity.java
@@ -1,18 +1,51 @@
 package com.ibea.fides.ui.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ibea.fides.BaseActivity;
 import com.ibea.fides.R;
 
-public class FaqActivity extends BaseActivity {
+import java.lang.reflect.Array;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class FaqActivity extends BaseActivity implements View.OnClickListener {
+
+    @Bind(R.id.textView_Email) TextView mTextViewEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+        ButterKnife.bind(this);
+
+        mTextViewEmail.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        if(view == mTextViewEmail){
+            String[] emails = {"ibeatechnology@gmail.com"};
+
+            composeEmail( emails , "Fides Support") ;
+        }
+    }
+
+    public void composeEmail(String[] addresses, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 
 }
