e9c192edb89a984fe29140692cf74bce484d1b96
Garrett Boggs <garrett.boggs@my.wheaton.edu>
2017-02-16 10:14:17 -0800

    delete line comments

--- app/src/main/java/com/ibea/fides/ui/activities/IntroActivity.java
+++ app/src/main/java/com/ibea/fides/ui/activities/IntroActivity.java
@@ -26,22 +26,12 @@ public class IntroActivity extends AppIntro {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Note here that we DO NOT use setContentView();
-
-        // Add your slide fragments here.
         // AppIntro will automatically generate the dots indicator and buttons.
         addSlide(IntroSlideFragment.newInstance(R.layout.fragment_intro_slide_welcome));
         addSlide(IntroSlideFragment.newInstance(R.layout.fragment_intro_slide_1_volunteer));
         addSlide(IntroSlideFragment.newInstance(R.layout.fragment_intro_slide_2_volunteer));
         addSlide(IntroSlideFragment.newInstance(R.layout.fragment_intro_slide_3));
 
-        //addSlide(AppIntroFragment.newInstance("Slide title", "Loong description here", R.drawable.common_full_open_on_phone, ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null) ));
-
-        // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest.
-        //addSlide(AppIntroFragment.newInstance(title, description, image, backgroundColor));
-
-        // OPTIONAL METHODS
         // Override bar/separator color.
         setBarColor(Color.parseColor("#5D4037"));
         setSeparatorColor(Color.parseColor("#EFEBE9"));
@@ -58,10 +48,6 @@ public class IntroActivity extends AppIntro {
 
         setFadeAnimation();
 
-        // Turn vibration on and set intensity.
-        // NOTE: you will probably need to ask VIBRATE permission in Manifest.
-        //setVibrate(true);
-        //setVibrateIntensity(30);
     }
 
     @Override
